36938dc87fcb074efb2333dfd750535c92b25586
sfaxit <saber.dridi79@gmail.com>
2024-03-30 12:59:12 +0100

    sfaxit-1: Adjust exception handler class

--- src/main/java/de/sfaxit/web/exception/ExceptionHandler.java
+++ src/main/java/de/sfaxit/web/exception/ExceptionHandler.java
@@ -2,17 +2,17 @@ package de.sfaxit.web.exception;
 
 import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class ExceptionHandler implements ExceptionMapper<WebApplicationException> {
+public class ExceptionHandler implements ExceptionMapper<MismatchedInputException> {
 	
 	@Override
-	public Response toResponse(WebApplicationException e) {
-		// Set the JSR-303 error into JSON format.
+	public Response toResponse(final MismatchedInputException e) {
 		return Response.status(BAD_REQUEST)
 		               .entity(e.getMessage())
 		               .build();
